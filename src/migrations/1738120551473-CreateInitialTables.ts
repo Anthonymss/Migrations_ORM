47070f3113221f22bdba69cd8fdd6639f19cfbb2
Anthonyy Choque <giananthoych@outlook.es>
2025-01-28 22:16:36 -0500

    first commit

--- src/migrations/1738120551473-CreateInitialTables.ts
+++ src/migrations/1738120551473-CreateInitialTables.ts
@@ -1,15 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Prueba1738120232427 implements MigrationInterface {
-    name = 'Prueba1738120232427'
+export class CreateInitialTables1738120551473 implements MigrationInterface {
+    name = 'CreateInitialTables1738120551473'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "profile" ("id" SERIAL NOT NULL, "bio" text NOT NULL, "isActive" boolean NOT NULL DEFAULT true, CONSTRAINT "PK_3dd8bfc97e4a77c70971591bdcb" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "username" character varying(100) NOT NULL, "createdAt" date NOT NULL, "profileId" integer, CONSTRAINT "REL_9466682df91534dd95e4dbaa61" UNIQUE ("profileId"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "category" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "isActive" boolean NOT NULL DEFAULT true, CONSTRAINT "PK_9c4e4a89e3674fc9f382d733f03" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "product" ("id" SERIAL NOT NULL, "name" character varying(100) NOT NULL, "price" integer NOT NULL, "categoryId" integer, CONSTRAINT "PK_bebc9158e480b949565b4dc7a82" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "student" ("id" SERIAL NOT NULL, "dateOfBirth" date NOT NULL, CONSTRAINT "PK_3d8016e1cb58429474a3c041904" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "course" ("id" SERIAL NOT NULL, "title" character varying(100) NOT NULL, "description" text, CONSTRAINT "PK_bf95180dd756fd204fb01ce4916" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "student" ("id" SERIAL NOT NULL, "dateOfBirth" date NOT NULL, CONSTRAINT "PK_3d8016e1cb58429474a3c041904" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "student_courses_course" ("studentId" integer NOT NULL, "courseId" integer NOT NULL, CONSTRAINT "PK_14a911a16ab76c78f1fe6368a92" PRIMARY KEY ("studentId", "courseId"))`);
         await queryRunner.query(`CREATE INDEX "IDX_29e49d9ad51ffb927f488f0802" ON "student_courses_course" ("studentId") `);
         await queryRunner.query(`CREATE INDEX "IDX_d13666d470035a399961e1d08c" ON "student_courses_course" ("courseId") `);
@@ -27,8 +27,8 @@ export class Prueba1738120232427 implements MigrationInterface {
         await queryRunner.query(`DROP INDEX "public"."IDX_d13666d470035a399961e1d08c"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_29e49d9ad51ffb927f488f0802"`);
         await queryRunner.query(`DROP TABLE "student_courses_course"`);
-        await queryRunner.query(`DROP TABLE "course"`);
         await queryRunner.query(`DROP TABLE "student"`);
+        await queryRunner.query(`DROP TABLE "course"`);
         await queryRunner.query(`DROP TABLE "product"`);
         await queryRunner.query(`DROP TABLE "category"`);
         await queryRunner.query(`DROP TABLE "user"`);
